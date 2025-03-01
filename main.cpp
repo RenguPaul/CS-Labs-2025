40619f976bcfacfa5cb820c18aaac6f21ff70058
Pavel Chuiko <pavelatch@gmail.com>
2025-03-01 21:31:12 +0300

    add some files

--- main.cpp
+++ main.cpp
@@ -2,6 +2,48 @@
 #include "mymath/mymath.h"
 
 int interactive() {
+    char continueExecution = 'y';
+    while (continueExecution == 'y' || continueExecution == 'Y') {
+        int a, b;
+        char operation;
+
+        std::cout << "Введите первое число (a): ";
+        std::cin >> a;
+        std::cout << "Введите второе число (b): ";
+        std::cin >> b;
+        std::cout << "Выберите операцию (+, -, *, /, ^): ";
+        std::cin >> operation;
+
+        switch (operation) {
+            case '+':
+                std::cout << "Sum(a, b) = " << mymath::Sum(a, b) << std::endl;
+                break;
+            case '-':
+                std::cout << "Substraction(a, b) = " << mymath::Substraction(a, b) << std::endl;
+                break;
+            case '*':
+                std::cout << "Product(a, b) = " << mymath::Product(a, b) << std::endl;
+                break;
+            case '/':
+                if (b != 0) {
+                    std::cout << "Division(a, b) = " << mymath::Division(a, b) << std::endl;
+                } else {
+                    std::cout << "Ошибка: деление на ноль!" << std::endl;
+                }
+                break;
+            case '^':
+                std::cout << "RaisingToPower(a, b) = " << mymath::RaisingToPower(a, b) << std::endl;
+                break;
+            default:
+                std::cout << "Неизвестная операция!" << std::endl;
+                break;
+        }
+
+        std::cout << "Хотите продолжить? (y/n): ";
+        std::cin >> continueExecution;
+    }
+
+    std::cout << "Программа завершена." << std::endl;
     return 0;
 }
 
@@ -16,7 +58,6 @@ int demo() {
     std::cout << mymath::Division(a, b) << " " << "Division(a, b) - деление a на b\n";
     std::cout << mymath::RaisingToPower(a, b) << " " << "RaisingToPower(a, b) - возведение  в степень a на b\n";
     return 0;
-
 }
 
 int main(int argc, char *argv[]) {
@@ -24,13 +65,12 @@ int main(int argc, char *argv[]) {
 
     // Если i передается в качестве аргумента, то программу
     // необходимо запустить в интерактивном режиме
-    if ((argc == 2) && argv[1] == "i") {
+    if ((argc == 2) && std::string(argv[1]) == "i") {
         isInteractive = true;
     }
 
     if (isInteractive) {
         return interactive();
-
     } else {
         return demo();
     }
