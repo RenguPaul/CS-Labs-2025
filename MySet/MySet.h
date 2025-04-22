8850583e97ba0942e3d77dfbbdf6e8cdf30459e6
Pavel Chuiko <pavelatch@gmail.com>
2025-04-23 00:28:54 +0300

    done lab_5

--- MySet/MySet.h
+++ MySet/MySet.h
@@ -1,7 +1,8 @@
 #ifndef MYSET_H
 #define MYSET_H
 
-#include "MyVector.h"
+#include "/home/pavel/BAUMAN_PROJECTS/CS-2025-PR/CS-Labs-2025/MyVector/MyVector.h"
+#include <iostream>
 
 template <typename T>
 class MySet : public MyVector<T> {
@@ -21,6 +22,10 @@ public:
     MySet<T>& operator-=(const MySet<T>& other);
     MySet<T>& operator*=(const MySet<T>& other);
     bool operator==(const MySet<T>& other) const;
+
+    template <typename U>
+    friend std::ostream& operator<<(std::ostream& os, const MySet<U>& set);
+
 };
 
 template <>
@@ -41,6 +46,283 @@ public:
     MySet<char*>& operator-=(const MySet<char*>& other);
     MySet<char*>& operator*=(const MySet<char*>& other);
     bool operator==(const MySet<char*>& other) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const MySet<char*>& set);
 };
+
+#include "MySet.h"
+#include <cstring>
+#include <algorithm>
+
+template <typename T>
+int MySet<T>::q_find(const T& element) const {
+    int left = 0;
+    int right = static_cast<int>(this->size) - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (this->pdata[mid] == element) {
+            return mid;
+        }
+        if (this->pdata[mid] < element) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+template <typename T>
+bool MySet<T>::is_element(const T& element) const {
+    return q_find(element) != -1;
+}
+
+template <typename T>
+void MySet<T>::add_element(const T& element) {
+    if (!is_element(element)) {
+        int index = 0;
+        while (index < static_cast<int>(this->size) &&
+               this->pdata[index] < element) {
+            index++;
+        }
+
+        if (this->size == this->max_size) {
+            this->resize(this->max_size ? this->max_size * 2 : 1);
+        }
+        for (int i = static_cast<int>(this->size); i > index; --i) {
+            this->pdata[i] = this->pdata[i-1];
+        }
+
+        this->pdata[index] = element;
+        this->size++;
+    }
+}
+
+template <typename T>
+void MySet<T>::delete_element(const T& element) {
+    int index = q_find(element);
+    if (index != -1) {
+        for (size_t i = index; i < this->size - 1; ++i) {
+            this->pdata[i] = this->pdata[i + 1];
+        }
+        this->size--;
+
+        if (this->size < this->max_size / 4 && this->max_size > 1) {
+            this->resize(this->max_size / 2);
+        }
+    }
+}
+
+
+template <typename T>
+bool MySet<T>::operator==(const MySet<T>& other) const {
+    if (this->size != other.size) {
+        return false;
+    }
+
+    for (size_t i = 0; i < this->size; ++i) {
+        if (this->pdata[i] != other.pdata[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template <typename T>
+MySet<T> MySet<T>::operator+(const MySet<T>& other) const {
+    MySet<T> newSet;
+
+    for (size_t i = 0; i < this->size; ++i) {
+        newSet.add_element(this->pdata[i]);
+    }
+
+    for (size_t i = 0; i < other.size; ++i) {
+        if (!this->is_element(other.pdata[i])) {
+            newSet.add_element(other.pdata[i]);
+        }
+    }
+
+    return newSet;
+}
+
+template <typename T>
+MySet<T> MySet<T>::operator-(const MySet<T>& other) const {
+    MySet<T> newSet;
+
+    for (size_t i = 0; i < this->size; ++i) {
+        if (!other.is_element(this->pdata[i])) {
+            newSet.add_element(this->pdata[i]);
+        }
+    }
+
+    return newSet;
+}
+
+template <typename T>
+MySet<T> MySet<T>::operator*(const MySet<T>& other) const {
+    MySet<T> newSet;
+
+    for (size_t i = 0; i < this->size; ++i) {
+        if (other.is_element(this->pdata[i])) {
+            newSet.add_element(this->pdata[i]);
+        }
+    }
+
+    return newSet;
+}
+
+template <typename T>
+MySet<T>& MySet<T>::operator*=(const MySet<T>& other) {
+    *this = *this * other;
+    return *this;
+}
+
+template <typename T>
+MySet<T>& MySet<T>::operator+=(const MySet<T>& other) {
+    *this = *this + other;
+    return *this;
+}
+
+template <typename T>
+MySet<T>& MySet<T>::operator-=(const MySet<T>& other) {
+    *this = *this - other;
+    return *this;
+}
+
+int MySet<char*>::q_find(const char* element) const {
+    int left = 0;
+    int right = static_cast<int>(this->size) - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        int cmp = strcmp(this->pdata[mid], element);
+
+        if (cmp == 0) {
+            return mid;
+        }
+        if (cmp < 0) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+bool MySet<char*>::is_element(const char* element) const {
+    return q_find(element) != -1;
+}
+
+void MySet<char*>::add_element(const char* element) {
+    if (!is_element(element)) {
+        int index = 0;
+        while (index < static_cast<int>(this->size) &&
+               strcmp(this->pdata[index], element) < 0) {
+            index++;
+        }
+
+        if (this->size == this->max_size) {
+            this->resize(this->max_size ? this->max_size * 2 : 1);
+        }
+
+        for (int i = static_cast<int>(this->size); i > index; --i) {
+            this->pdata[i] = this->pdata[i-1];
+        }
+
+        this->pdata[index] = new char[strlen(element) + 1];
+        strcpy(this->pdata[index], element);
+        this->size++;
+    }
+}
+
+void MySet<char*>::delete_element(const char* element) {
+    int index = q_find(element);
+    if (index != -1) {
+        delete[] this->pdata[index];
+
+        for (size_t i = index; i < this->size - 1; ++i) {
+            this->pdata[i] = this->pdata[i + 1];
+        }
+        this->size--;
+
+        if (this->size < this->max_size / 4 && this->max_size > 1) {
+            this->resize(this->max_size / 2);
+        }
+    }
+}
+
+bool MySet<char*>::operator==(const MySet<char*>& other) const {
+    if (this->size != other.size) {
+        return false;
+    }
+
+    for (size_t i = 0; i < this->size; ++i) {
+        if (strcmp(this->pdata[i], other.pdata[i]) != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+MySet<char*> MySet<char*>::operator+(const MySet<char*>& other) const {
+    MySet<char*> newSet;
+
+    for (size_t i = 0; i < this->size; ++i) {
+        newSet.add_element(this->pdata[i]);
+    }
+
+    for (size_t i = 0; i < other.size; ++i) {
+        if (!newSet.is_element(other.pdata[i])) {
+            newSet.add_element(other.pdata[i]);
+        }
+    }
+
+    return newSet;
+}
+
+MySet<char*> MySet<char*>::operator-(const MySet<char*>& other) const {
+    MySet<char*> newSet;
+
+    for (size_t i = 0; i < this->size; ++i) {
+        if (!other.is_element(this->pdata[i])) {
+            newSet.add_element(this->pdata[i]);
+        }
+    }
+
+    return newSet;
+}
+
+MySet<char*> MySet<char*>::operator*(const MySet<char*>& other) const {
+    MySet<char*> newSet;
+
+    for (size_t i = 0; i < this->size; ++i) {
+        if (other.is_element(this->pdata[i])) {
+            newSet.add_element(this->pdata[i]);
+        }
+    }
+
+    return newSet;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const MySet<T>& set) {
+    os << '{' << ' ';
+    for(size_t i=0; i<set.size; ++i) {
+        os << set.pdata[i] << ' ';
+    }
+    os << '}';
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const MySet<char*>& set) {
+    os << '{' << ' ';
+    for(size_t i=0; i<set.size; ++i) {
+        os << set.pdata[i] << ' ';
+    }
+    os << '}';
+    return os;
+}
+
 
 #endif // MYSET_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include "MyVector/MyVector.h"
 #include "MySet/MySet.h"
 
+
 int main() {
+    MySet<int>aa{};
+    MySet<int>gg{};
+    aa.add_element(1);
+    aa.add_element(4);
+    aa.add_element(5);
+    aa.add_element(6);
+    gg.add_element(1);
+    gg.add_element(2);
+    gg.add_element(3);
+    gg.add_element(4);
+    std::cout << aa -gg << std::endl;
+    std::cout << aa * gg << std::endl;
+    std::cout << aa + gg << std::endl;
+    return 0;
 
 }
